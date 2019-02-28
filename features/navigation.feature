31bf4a9f878d9dd5c1ff0b1429e1b2f7be8e0225
Abner Qian <LargeBagel@gmail.com>
2019-02-28 13:13:20 -0800

    Added a few extra steps to cleanly separate some actions and make them more reusable. Finished step definitions.

--- features/navigation.feature
+++ features/navigation.feature
@@ -1,11 +1,13 @@
-Feature: Adding an item to cart
+Feature: Adding an item to the shopping cart
 Scenario:
   Given We navigate to the homepage
   When We log in
-  Then The cart will be empty
+  Then The cart will say "Cart (empty)"
   When We click on the "Women" tab in the top menu
   And We expand the "Tops" category in the side menu
   And We expand the "Dresses" category in the side menu
   Then The heading counter will say "There are 7 products."
   When We add one item to the cart
-  Then The cart will have 1 product
+  Then The post-add-to-cart dialogue box will pop up
+  And The right of the dialogue box will say "There is 1 item in your cart."
+  And The cart will say "Cart 1 Product"
--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -14,9 +14,9 @@ When("We log in") do
   driver.find_element(:id, "SubmitLogin").click
 end
 
-Then("The cart will be empty") do
+Then("The cart will say {string}") do |string|
   cart = driver.find_element(:class, "shopping_cart")
-  expect(cart.text).to eq("Cart (empty)")
+  expect(cart.text).to eq(string)
 end
 
 When("We click on the {string} tab in the top menu") do |string|
@@ -37,11 +37,29 @@ Then("The heading counter will say {string}") do |string|
 end
 
 When("We add one item to the cart") do
-  pending # Write code here that turns the phrase above into concrete actions
+  product = driver.find_element(:class, "product-container")
+  driver.execute_script("arguments[0].scrollIntoView();", product)
+  driver.action.move_to(product).perform
+
+  wait = Selenium::WebDriver::Wait.new(:timeout => 1)
+  add_to_cart = wait.until do
+    product.find_element(:link_text, "Add to cart")
+  end
+  add_to_cart.click
 end
 
-Then("The cart will have {int} product") do |int|
-  pending # Write code here that turns the phrase above into concrete actions
+Then("The post-add-to-cart dialogue box will pop up") do
+  wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+  wait.until do
+    popup = driver.find_element(:id, "layer_cart")
+    !popup.text.empty?
+  end
+end
+
+Then("The right of the dialogue box will say {string}") do |string|
+  popup_right_col = driver.find_element(:class, "layer_cart_cart")
+  cart_text = popup_right_col.find_element(:class, "ajax_cart_product_txt").text
+  expect(cart_text).to eq(string)
 end
 
 After do
